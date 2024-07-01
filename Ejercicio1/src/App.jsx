d8dfaa00d4ca7662e4ad38fcfd39b139bcad31e4
Nardy <nardy.allstars@gmail.com>
2024-07-01 21:51:34 +0200

    home page created

--- Ejercicio1/src/App.jsx
+++ Ejercicio1/src/App.jsx
@@ -1,11 +1,15 @@
-import './App.css'
+import supabase from './supabase/config'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
 
 function App() {
-
-
   return (
     <>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
     </>
+
   )
 }
 
---
+++ Ejercicio1/src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+
+
+function HomePage() {
+
+    return (
+        <><div>asdasdasd</div>
+        </>
+    )
+}
+export default HomePage;
